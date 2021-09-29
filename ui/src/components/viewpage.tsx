c17f5aa58625b8a5ac1754298f3d1b1f6a683b7c
Yoshimura Hiroki <hiroshuke0624@gmail.com>
2021-09-29 23:15:35 +0900

    ビューアー作成

--- ui/src/components/viewpage.tsx
+++ ui/src/components/viewpage.tsx
@@ -2,15 +2,14 @@ import { Viewer } from "./viewer";
 import { useState } from 'react';
 import { Selection } from '../parts/select';
 import { Dcmupload } from '../parts/dcmUpload';
-//@ts-ignore
-import sampledcm from '../images/80_65.dcm';
 
-type imageIds = (any[] | null);
-type activetool = string
+const sampledcm: any = require('../images/80_65.dcm').default;
+
+
 
 export const Viewpage = (): JSX.Element => {
-    const [imageIds, setImageIds] = useState<imageIds>(null);
-    const [activeTool, setActiveTool] = useState<activetool>('Wwwc');
+    const [imageIds, setImageIds] = useState<string[]|null>(null);
+    const [activeTool, setActiveTool] = useState<string>('Wwwc');
     // FORM
 
     const SetImageIds = (images: string[]): void => {
@@ -46,7 +45,7 @@ export const Viewpage = (): JSX.Element => {
                 className="body_btn"
                 type="button"
                 onClick={() => {
-                    setImageIds(["dicomweb:" + sampledcm])
+                    setImageIds(["dicomweb:"+sampledcm])
                 }}
             >
                 web上のものを選択
