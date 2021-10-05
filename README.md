b2c711e29a76e1536f1135ea592729c91caa040b
unonao <naokiuno634@gmail.com>
2021-10-05 23:09:52 +0900

    がんばった

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # 肺炎検出ビジュアライザ
+
+## 全体をお試ししたい時
+
+```
+docker-compose up --build
+```
+
+## バックエンドだけの実行について
+
+### イメージをビルド
+
+```
+docker build -t name:tag {Dockerfileがある場所のディレクトリのパス}
+```
+
+例：
+
+```
+docker build -t backend:latest .
+```
+
+### イメージの起動
+
+```
+docker run -it -p 8080:8080 backend
+```
+
+## その他
+
+dockerfile の元のイメージ（uvicorn-gunicorn+fastapi)
+
+[uvicorn-gunicorn-fastapi-docker](https://github.com/tiangolo/uvicorn-gunicorn-fastapi-docker)
+
+yolov5 を app の中に入れて、requirements.txt をコピーしてインストールするように、dockerfile を以下を参考に書き換える。
+
+```Dockerfile
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.9
+COPY ./requirements.txt /app/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
+COPY ./app /app
+```
--- app/app/main.py
+++ app/app/main.py
@@ -1,9 +1,7 @@
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, ORJSONResponse, RedirectResponse, FileResponse
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
 import numpy as np
+import shutil
 import pydicom
 from PIL import Image
 from typing import List
@@ -12,97 +10,30 @@ import os
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
-
-
-class Result(BaseModel):
-    code: int = 0
-    message: str = ''
-    items: List[Item] = None
-
-
-class User(BaseModel):
-    username: str
-    hostname: str
-
-
-@app.get("/api/user", response_model=User)
-def read_user():
-    import socket
-    import getpass
-    return {
-        "username": getpass.getuser(),
-        "hostname": socket.gethostname()
-    }
-
-
-@app.get("/api/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/api/items/{item_id}", response_model=Result)
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/api/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.get("/api/fileupload/filelist", response_class=ORJSONResponse)
-async def get_filelist(request: Request):
-    '''docstring
-    アップロードされたファイルの一覧を取得する
-    '''
-
-    def get_extention(filePath):
-        '''docstring
-        ファイルパスから拡張子を取得する
-        '''
-        ex = os.path.splitext(filePath)
-        return ex[len(ex)-1]
-
-    uploadedpath = "./uploads"
-    files = os.listdir(uploadedpath)
-    filelist = [{
-        "filename":f, 
-        "filesize": os.path.getsize(os.path.join(uploadedpath, f)),    
-        "extention": get_extention(f),
-    } for f in files if os.path.isfile(os.path.join(uploadedpath, f))]
-    return filelist
-
-@app.post("/api/",response_class=ORJSONResponse)
-async def get_filelist(formData,):
+@app.post("/api/")
+async def get_filelist(file: UploadFile = File(...)):
     '''docstring
     ファイルを取得する
     '''
-    filejson = formData.avatar
-    filename = filejson.name
+    filename = file.filename
     uploadedpath = f"{os.getcwd()}/uploads/{filename}"
+    with open(uploadedpath, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
     data = pydicom.read_file(uploadedpath)
     img = data.pixel_array
-    image_path = f"{os.getcwd()}/Image"
-    try:
-        os.makedirs(image_path)
-    except FileExistsError:
-        pass
+    image_path = f"{os.getcwd()}/images"
     jpeg_img = img.astype('u1')
     save_img = Image.fromarray(jpeg_img).convert('L')
-    save_path = image_path+'/'+'sample.jpg'
-    save_img.save(save_path,quality=100)
+    save_path = image_path + '/' + 'sample.jpg'
+    save_img.save(save_path, quality=100)
 
-    
-    files = os.listdir(uploadedpath)
+    files = os.listdir(image_path)
     filelist = [{
-        "filename":f,
-        "filesize": os.path.getsize(os.path.join(uploadedpath, f)),    
-    } for f in files if os.path.isfile(os.path.join(uploadedpath, f))]
+        "filename": f,
+        "filesize": os.path.getsize(os.path.join(image_path, f)),
+    } for f in files if os.path.isfile(os.path.join(image_path, f))]
     return filelist
-
 
 
 if __name__ == "__main__":
